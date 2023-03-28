bea7f12dd2c44511fc3ecfbb2b5e3a0a168ea55c
ningosi <nicotwendelee@gmail.com>
2023-03-28 20:50:43 +0300

    Initial work on the konza work configurations

---
+++ api/src/main/java/org/openmrs/module/konzaconfig/ConfigurationfortheKonzaprojectConfig.java
@@ -0,0 +1,21 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.konzaconfig;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Contains module's config.
+ */
+@Component("konzaconfig.ConfigurationfortheKonzaprojectConfig")
+public class ConfigurationfortheKonzaprojectConfig {
+	
+	public final static String MODULE_PRIVILEGE = "Configuration for the Konza project Privilege";
+}
