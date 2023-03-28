337c98872acc10f804fc2dbc808540bb993eb80f
ningosi <nicotwendelee@gmail.com>
2023-03-28 21:24:23 +0300

    Add a sample app in the module application context

--- omod/src/main/java/org/openmrs/module/konzaconfig/page/controller/RegistrationPageController.java
+++ omod/src/main/java/org/openmrs/module/konzaconfig/page/controller/RegistrationPageController.java
@@ -4,7 +4,7 @@ import org.openmrs.module.kenyaui.annotation.AppPage;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
 
-@AppPage("konzaconfig.home")
+@AppPage("konzaconfig.registration")
 public class RegistrationPageController {
 	
 	public void controller(UiUtils ui, PageModel model) {
