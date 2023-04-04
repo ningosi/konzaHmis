35864f6e3d9747f6d4e786f5d1b5933017ae0315
ningosi <nicotwendelee@gmail.com>
2023-04-04 16:09:19 +0300

    Addition of more apps on the home landing page

--- api/src/main/java/org/openmrs/module/konzaconfig/metadata/KonzaHmisSecurityMetadata.java
+++ api/src/main/java/org/openmrs/module/konzaconfig/metadata/KonzaHmisSecurityMetadata.java
@@ -21,6 +21,10 @@ public class KonzaHmisSecurityMetadata extends AbstractMetadataBundle {
 		public static final String APP_OPD_APP = "App: konzaconfig.opd";
 		
 		public static final String APP_LAB_APP = "App: konzaconfig.lab";
+		
+		public static final String APP_IMAGING_APP = "App: konzaconfig.imaging";
+		
+		public static final String APP_PROCEDURE_APP = "App: konzaconfig.procedure";
 	}
 	
 	public static final class _Role {
@@ -38,11 +42,13 @@ public class KonzaHmisSecurityMetadata extends AbstractMetadataBundle {
 		install(privilege(_Privilege.APP_QUEUE_APP, "Able to access Konza HMIS queue application"));
 		install(privilege(_Privilege.APP_OPD_APP, "Able to access Konza HMIS OPD application"));
 		install(privilege(_Privilege.APP_LAB_APP, "Able to access Konza HMIS LAB application"));
+		install(privilege(_Privilege.APP_IMAGING_APP, "Able to access Konza HMIS imaging application"));
+		install(privilege(_Privilege.APP_PROCEDURE_APP, "Able to access Konza HMIS procedure application"));
 		install(role(
 		    _Role.APPLICATION_HMIS_MODULE,
 		    "Can access Konza HMIS module application functionalities",
 		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
 		    idSet(_Privilege.APP_REG_APP, _Privilege.APP_TRIAGE_APP, _Privilege.APP_QUEUE_APP, _Privilege.APP_OPD_APP,
-		        _Privilege.APP_LAB_APP)));
+		        _Privilege.APP_LAB_APP, _Privilege.APP_IMAGING_APP, _Privilege.APP_PROCEDURE_APP)));
 	}
 }
